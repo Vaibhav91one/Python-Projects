8ee4ca59dc89b6b46038d6528fe1b19d02b8c13a
Vaibhav91one <vtking03@gmail.com>
2022-08-14 19:08:57 +0530

    First Commit

---
+++ countdown_timer.py
@@ -0,0 +1,16 @@
+import time
+
+
+def countdown(t):
+    while t:
+        minutes, seconds = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(minutes, seconds)
+        print('\r', timer, end="")
+        time.sleep(1)
+        t -= 1
+    print("Fire in the hole!")
+
+
+t = input("Enter time in seconds: ")
+countdown(int(t))
+
